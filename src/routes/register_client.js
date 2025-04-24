50842dab9f2a8ce8ff23e1980dd569382934fc3c
Pewter47 <pedro.gama.afonso@gmail.com>
2025-04-24 16:26:46 +0100

    cleanup4

--- src/routes/register_client.js
+++ src/routes/register_client.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { register, check } from '../controllers/clientsController.js';
+import { register } from '../controllers/clientsController.js';
 const router = express.Router();
 router.post('/register', register);
 export default router;
--- src/routes/register_user.js
+++ src/routes/register_user.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register, login } from "../controllers/usersController.js";
+import { register } from "../controllers/usersController.js";
 const router = express.Router();
 router.post("/register", register);
 export default router;
